4cd8cb7447360e692afb98c0daacfc1ed9bc92d2
jhicks2306 <jhicks2306@gmail.com>
2023-12-29 22:12:17 +0000

    Adjust api to work in docker

--- model_api/app.py
+++ model_api/app.py
@@ -3,8 +3,8 @@ import vetiver
 import pins
 
 
-b = pins.board_folder('../../models/do4ds', allow_pickle_read=True)
-v = VetiverModel.from_pin(b, 'penguin_model', version = '20231229T163240Z-73a81')
+b = pins.board_folder('/models/do4ds', allow_pickle_read=True)
+v = VetiverModel.from_pin(b, 'penguin_model')
 
 vetiver_api = vetiver.VetiverAPI(v)
 api = vetiver_api.app
