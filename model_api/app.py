7c5547607f9ac02b9f57b2192eb80f36696d6ecb
jhicks2306 <jhicks2306@gmail.com>
2024-01-10 14:48:07 +0000

    Update GitHub actions to reference AWS

--- model_api/app.py
+++ model_api/app.py
@@ -1,10 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from vetiver import VetiverModel
 import vetiver
 import pins
 
 
-b = pins.board_folder('/models/do4ds', allow_pickle_read=True)
-v = VetiverModel.from_pin(b, 'penguin_model')
+b = pins.board_s3('do4ds-lab-s3', allow_pickle_read=True)
+v = VetiverModel.from_pin(b, 'penguin_model', version = '20240110T123044Z-73a81')
 
 vetiver_api = vetiver.VetiverAPI(v)
 api = vetiver_api.app
